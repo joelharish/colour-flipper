ac957e12305cd865dc0eb903d3f65a5810172176
joelharish <joelharish325@gmail.com>
2024-01-04 12:07:53 +0530

    first commit

---
+++ src/Colour.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+
+const Colour = () => {
+  return (
+    <div className='container'>
+      <div className='middleContent'>
+        <h2>Colour : </h2>
+      </div>
+      <button>Flip</button>
+    </div>
+  )
+}
+
+export default Colour
